6b2f1b2cd80d73ce120765f478b5bb7b9fb815c2
slachhar <shefali.lachhar@gmail.com>
2019-07-26 18:06:13 +0530

    Climbing LeaderBoard

--- HackerRankProblems/Program.cs
+++ HackerRankProblems/Program.cs
@@ -6,16 +6,16 @@ namespace HackerRankProblems
 {
 	public class Program
 	{
-		private static void Main(string[] args)
-		{
-			int[][] s = new int[3][];
+		//private static void Main(string[] args)
+		//{
+		//	int[][] s = new int[3][];
 
-			for (int i = 0; i < 3; i++)
-			{
-				s[i] = Array.ConvertAll(Console.ReadLine().Split(' '), sTemp => Convert.ToInt32(sTemp));
-			}
+		//	for (int i = 0; i < 3; i++)
+		//	{
+		//		s[i] = Array.ConvertAll(Console.ReadLine().Split(' '), sTemp => Convert.ToInt32(sTemp));
+		//	}
 
-			int result = FormingMagicSquare.formingMagicSquare(s);
-		}
+		//	int result = FormingMagicSquare.formingMagicSquare(s);
+		//}
 	}
 }
