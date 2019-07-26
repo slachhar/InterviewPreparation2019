b33c8ab4c073c9fdc31287e36940136858d0e7a2
slachhar <shefali.lachhar@gmail.com>
2019-07-26 16:15:43 +0530

    Forming Magic Square

---
+++ HackerRankProblems/FormingMagicSquare.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems
+{
+	public static class FormingMagicSquare
+	{
+		//5 3 4
+		//1 5 8
+		//6 4 2
+		public static int formingMagicSquare(int[][] s)
+		{
+			int[,,] magicsquarePossibilties = new int[8, 3, 3]  { { { 8, 1, 6 }, { 3,5,7}, { 4, 9, 2 } },
+																{ { 6,1,8},{ 7,5,3},{ 2,9,4 } },
+																{ { 4,9,2},{ 3,5,7},{ 8,1,6 } },
+																{ { 2,9,4},{ 7,5,3},{ 6,1,8} },
+																{ { 8,3,4},{ 1,5,9},{ 6,7,2 } },
+																{ { 4,3,8},{ 9,5,1},{ 2,7,6} },
+																{ { 6,7,2},{ 1,5,9},{ 8,3,4 } },
+																{ { 2,7,6},{ 9,5,1},{ 4,3,8 } } };
+			//{8,1,6,3,5,7,4,9,2 },
+
+			List<int> TotalCost = new List<int>();
+			for (int k = 0; k < magicsquarePossibilties.GetLength(0); k++)
+			{
+				TotalCost.Add(0);
+				for (int i = 0; i < s.Length; i++)
+				{
+					for (int j = 0; j < s[i].Length; j++)
+					{
+						TotalCost[k] += Math.Abs(magicsquarePossibilties[k, i, j] - s[i][j]);
+					}
+				}
+			}
+
+			int min = TotalCost.Min();
+			return min;
+		}
+
+	}
+}
--- HackerRankProblems/Program.cs
+++ HackerRankProblems/Program.cs
@@ -1,40 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HackerRankProblems
 {
 	public class Program
 	{
-		//5 3 4
-		//1 5 8
-		//6 4 2
-		static int formingMagicSquare(int[][] s)
+		private static void Main(string[] args)
 		{
-			int[,] magicsquarePossibilties = new int[8, 9]{
-												{8,1,6,3,5,7,4,9,2 },
-												{6,1,8,7,5,3,2,9,4 },
-												{4,9,2,3,5,7,8,1,6 },
-												{2,9,4,7,5,3,6,1,8 },
-												{8,3,4,1,5,9,6,7,2 },
-												{4,3,8,9,5,1,2,7,6 },
-												{6,7,2,1,5,9,8,3,4 },
-												{2,7,6,9,5,1,4,3,8 }};
-			int TotalCost = 0;
-			for (int i = 0; i < s.Length; i++)
-			{
-				for (int j = 0; j < s[i].Length; j++)
-				{
-					//TotalCost += (magicsquarePossibilties[])
-				}
-			}
-			return 0;
-		}
-
-		static void Main(string[] args)
-		{			
 			int[][] s = new int[3][];
 
 			for (int i = 0; i < 3; i++)
@@ -42,7 +15,7 @@ namespace HackerRankProblems
 				s[i] = Array.ConvertAll(Console.ReadLine().Split(' '), sTemp => Convert.ToInt32(sTemp));
 			}
 
-			int result = formingMagicSquare(s);
+			int result = FormingMagicSquare.formingMagicSquare(s);
 		}
 	}
 }
